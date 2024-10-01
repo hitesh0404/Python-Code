1db4c13d4f2273a06b40da1dc3a6e4bc7d9c2bbb
hitesh <hiteshkukaramprajapati@gmail.com>
2024-10-01 09:28:01 +0530

    Added has a relation between user and post

--- Batch 420/Project_on_OOPS/project2/main.py
+++ Batch 420/Project_on_OOPS/project2/main.py
@@ -2,11 +2,17 @@ from user import User
 import uuid
 stop=False
 while(stop!=True):
-    data = input('''
+    data = int(input('''
             If You want to add user 
             press 1...
             if You want to exit 
-            press 2...''')
+            press 2...
+            if You want to print all the availble user
+            press 3...
+            if You want to add post
+            press 4...
+                     
+            '''        ))
     if data==1:
         user_id = uuid.uuid4()
         username = input('Enter UserName: ')
@@ -15,8 +21,23 @@ while(stop!=True):
         age = input('Enter Age: ')
         password = input('Enter password: ')
         confirm_password = input('Confirm password: ')
-        u =User(user_id,username,first_name,last_name,
-                age,password,confirm_password)
+        if password!=confirm_password:
+            print('mismatch password')
+        else:
+            u =User(user_id,username,first_name,last_name,
+                age,password)
     elif data==2:
         break
+    elif data ==3 :
+        for i in User.all_user:
+            print(i)
+    elif data ==4 :
+        post_id =uuid.uuid4()
+        username = input('enter username')
+        for i in User.all_user:
+            if i.username == username:
+                title = input('enter post title')
+                data = input('enter post data')
+                i.create_post(post_id,title,data)
+
       
--- Batch 420/Project_on_OOPS/project2/user.py
+++ Batch 420/Project_on_OOPS/project2/user.py
@@ -1,18 +1,29 @@
+class Post:
+    def __init__(self,id,title,data) -> None:
+        self.id = id
+        self.title  = title
+        self.data = data
+
 class User:
-    def __init__(self,user_id,username,first_name,last_name,age,password,confirm_password) -> None:
-        if password!=confirm_password:
-            print('mismatch password')
-            return 'error: password is not matching'
+    all_user = []
+    def __init__(self,user_id,username,first_name,last_name,age,password) -> None:
         self.user_id = user_id
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.password = password
-    
-    def post(self,post_title,post_data):
-        pass
+        User.blood_color
+        User.all_user.append(self)
+        self.posts = []
+    def create_post(self,post_id,post_title,post_data):
+        post_obj = Post(post_id,post_title,post_data)
+        self.posts.append(post_obj)
 
+   
     def like(self,post_id):
         pass
 
+    def __str__(self) -> str:
+        return f'{self.user_id} {self.first_name} {self.last_name}'
+    
