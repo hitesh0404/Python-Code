289dcabc753d0f480ffcd649d68fad4690d516b6
hitesh <hiteshkukaramprajapati@gmail.com>
2024-10-10 12:05:03 +0530

    revision

--- Batch 420/Project_on_OOPS/project2/user.py
+++ Batch 420/Project_on_OOPS/project2/user.py
@@ -7,13 +7,13 @@ class Post:
 class User:
     all_user = []
     def __init__(self,user_id,username,first_name,last_name,age,password) -> None:
+        """this is constructor of user class"""
         self.user_id = user_id
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.password = password
-        User.blood_color
         User.all_user.append(self)
         self.posts = []
     def create_post(self,post_id,post_title,post_data):
@@ -27,3 +27,6 @@ class User:
     def __str__(self) -> str:
         return f'{self.user_id} {self.first_name} {self.last_name}'
     
+
+
+sameer = User()
